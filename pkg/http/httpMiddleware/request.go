c400c32ac9dba0b3a514277e620d54fed0eb6230
o.kulzhambekov <o.kulzhambekov@skiftrade.kz>
2024-03-29 10:24:46 +0500

    +

--- pkg/http/httpMiddleware/request.go
+++ pkg/http/httpMiddleware/request.go
@@ -9,11 +9,11 @@ import (
 // RequestMiddleware Middleware заполняющий данные запроса
 func RequestMiddleware(appInfo *config.AppInfo) gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		appInfo.RequestId = c.GetHeader(constants.RequestIdHeaderName)
 		// если request id не пришел с заголовком, генерим его, чтобы прокидывать дальше при http запросах
 		if appInfo.RequestId == "" {
 			appInfo.GenerateRequestId()
+			c.Request.Header.Add(constants.RequestIdHeaderName, appInfo.RequestId)
 		}
 
 		appInfo.LanguageCode = c.GetHeader(constants.LanguageHeaderName)
