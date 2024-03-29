bdd182ead9c0e3c65bf713d6856ce8b1b249a784
o.kulzhambekov <o.kulzhambekov@skiftrade.kz>
2024-03-29 10:29:43 +0500

    +

--- pkg/http/httpHelpers/http_response_helper.go
+++ pkg/http/httpHelpers/http_response_helper.go
@@ -1,8 +1,11 @@
 package httpHelpers
 
 import (
+	"fmt"
 	"github.com/exgamer/go-sdk/pkg/exception"
 	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/mapstructure"
+	"net/http"
 )
 
 func ErrorResponse(c *gin.Context, statusCode int, err error, context map[string]any) {
@@ -16,4 +19,29 @@ func AppExceptionResponse(c *gin.Context, exception *exception.AppException) {
 
 func SuccessResponse(c *gin.Context, data any) {
 	c.Set("data", data)
+}
+
+func FormattedResponse(c *gin.Context) {
+	for _, err := range c.Errors {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+
+		return
+	}
+
+	appExceptionObject, exists := c.Get("exception")
+	fmt.Printf("%+v\n", appExceptionObject)
+
+	if !exists {
+		data, _ := c.Get("data")
+
+		c.JSON(http.StatusOK, gin.H{"success": true, "data": data})
+
+		return
+	}
+
+	appException := exception.AppException{}
+	mapstructure.Decode(appExceptionObject, &appException)
+	fmt.Printf("%+v\n", appException)
+
+	c.JSON(appException.Code, gin.H{"message": appException.Error.Error(), "details": appException.Context})
 }
--- pkg/http/httpMiddleware/formatted_response.go
+++ pkg/http/httpMiddleware/formatted_response.go
@@ -1,11 +1,8 @@
 package httpMiddleware
 
 import (
-	"fmt"
-	"github.com/exgamer/go-sdk/pkg/exception"
+	"github.com/exgamer/go-sdk/pkg/http/httpHelpers"
 	"github.com/gin-gonic/gin"
-	"github.com/mitchellh/mapstructure"
-	"net/http"
 )
 
 // FormattedResponseMiddleware Middleware для обработки ответа
@@ -13,27 +10,6 @@ func FormattedResponseMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
 
-		for _, err := range c.Errors {
-			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-
-			return
-		}
-
-		appExceptionObject, exists := c.Get("exception")
-		fmt.Printf("%+v\n", appExceptionObject)
-
-		if !exists {
-			data, _ := c.Get("data")
-
-			c.JSON(http.StatusOK, gin.H{"success": true, "data": data})
-
-			return
-		}
-
-		appException := exception.AppException{}
-		mapstructure.Decode(appExceptionObject, &appException)
-		fmt.Printf("%+v\n", appException)
-
-		c.JSON(appException.Code, gin.H{"message": appException.Error.Error(), "details": appException.Context})
+		httpHelpers.FormattedResponse(c)
 	}
 }
