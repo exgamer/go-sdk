cfd9b33103dc1adcfbc97def0fc3220a8600873f
o.kulzhambekov <o.kulzhambekov@skiftrade.kz>
2024-04-15 18:20:06 +0500

    redis

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ SDK Tool for fast REST Go Application create
 
 Example Application https://github.com/exgamer/go-sdk-rest-template
 
-
+Gin rate limiter
+https://github.com/JGLTechnologies/gin-rate-limit
---
+++ pkg/database/redis_helper.go
@@ -0,0 +1,63 @@
+package database
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+// NewRedisHelper - Новый Хелпер для работы с редисом
+func NewRedisHelper[E interface{}](redisClient *redis.Client) *RedisHelper[E] {
+	return &RedisHelper[E]{
+		redisClient: redisClient,
+	}
+}
+
+// RedisHelper - Хелпер для работы с редисом
+type RedisHelper[E interface{}] struct {
+	redisClient *redis.Client
+	result      E
+}
+
+// GetByModel Возвращает значение по ключу
+func (redisHelper *RedisHelper[E]) GetByModel(key string) (*E, error) {
+	ctx := context.Background()
+	val, err := redisHelper.redisClient.Get(ctx, key).Result()
+
+	if err != nil && !errors.Is(err, redis.Nil) {
+		return nil, err
+	}
+
+	if val == "" {
+		return nil, nil
+	}
+
+	unMarshErr := json.Unmarshal([]byte(val), redisHelper.result)
+
+	if unMarshErr != nil {
+		return nil, unMarshErr
+	}
+
+	return &redisHelper.result, nil
+}
+
+// SetByModel Записывает значение по ключу
+func (redisHelper *RedisHelper[E]) SetByModel(key string, model *E, ttl time.Duration) error {
+	jsonModel, err := json.Marshal(model)
+
+	if err != nil {
+
+		return err
+	}
+
+	ctx := context.Background()
+	err = redisHelper.redisClient.Set(ctx, key, jsonModel, ttl).Err()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
